ca6723a03be0a6240d3c175d33575f517547950b
David Rojo <rojo@rojorevolution.com>
2021-01-12 12:44:01 -0600

    Changed API url to the one provided by Reagen as mine was not workingn properly

--- logic.js
+++ logic.js
@@ -1,10 +1,21 @@
 
 $(document).ready(function () {
 
+    //empty variable to hold search term
+    var searchTerms = ""
+    //empty variable to hold the number of records to show
+    var recordNumbers = ""
+    //empty variable to hold the number of records to the Start Year
     var yearStart = ""
+    //empty variable to hold the number of records to the End Year
     var yearEnd = ""
+    // We might need to append 0101 to the year
+    var yearAddition = "0101"
 
-    var queryURL = "https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=rqZFSP5bhXeXZG47FKvIzx7cpq5RG1Tp"
+
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=pNI9zJwwGgvD913UF1vxDKz8J3FC6yde"
+
+
 
     console.log(queryURL)
 
