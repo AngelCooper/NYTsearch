1c4701d3c2a87367c05ed528002dd1588a591004
ReaganG <rbgrunwald1997@gmail.com>
2021-01-12 14:30:57 -0600

    results will pop up

--- logic.js
+++ logic.js
@@ -15,13 +15,13 @@ $(document).ready(function () {
     //search input
     $(".search").on("click", function () {
         searchQuery = $("#search-input").val();
-        console.log(searchQuery);
+
         recordNumbers = $("#num-to-get").val();
-        console.log(recordNumbers);
+
         yearStart = $("#start-year").val();
-        console.log(yearStart);
+
         yearEnd = $("#end-year").val();
-        console.log(yearEnd);
+
 
         var queryURL = " https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + yearStart + "0101&end_date=" + yearEnd + "0101&q=" + searchQuery + "&api-key=pNI9zJwwGgvD913UF1vxDKz8J3FC6yde"
 
@@ -29,17 +29,43 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response)
+            var articles = response.response.docs;
+            $(articles).each(function (i, article) {
+                if(i >= +recordNumbers){ return; }
+                $(".articles-container").append(/*html*/`
+                <div class="article">
+                    <div class="index">${i + 1}</div>
+                    <h2 class="title">${article.headline.main}</h2>
+                    <br>
+                    <br>
+                    <br>
+                    <div class="author" >${article.byline.original}</div>
+                </div>
+                `)
+
+                
+            })
         })
-
-    });
-
-
-
-
-    $(".clear btn").on(function () {
-        localStorage.clear();
-        initPage()
+        //     $(".articles-container").append(/*html*/`
+        //     <div class="article">
+        //     <div class="index">1</div>
+        //     <h2 class="title">Mark Twain Did Something</h2>
+        //     <br>
+        //     <br>
+        //     <br>
+        //     <div class="author" >By: Reagan Grunwald</div>
+        // </div>
+        //     `)
     })
+
 });
+
+
+
+
+// $(".clear btn").on(function () {
+//     localStorage.clear();
+//     initPage()
+// })
+// });
 
