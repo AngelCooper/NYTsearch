5ff400a9a52fc61755f00c8655a18fcba4fbc903
ReaganG <rbgrunwald1997@gmail.com>
2021-01-12 13:50:14 -0600

    quick fixes

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
 
 
     //empty variable to hold search term
@@ -14,31 +14,36 @@ $(document).ready(function() {
 
 
 
-    var queryURL = " https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + yearStart + "0101&end_date=" + yearEnd + "0101&q=" + searchQuery + "&api-key=pNI9zJwwGgvD913UF1vxDKz8J3FC6yde"
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        // console.log(response)
-    })
 
-});
-  
+
+
     //search input
-    $(".search").on("click", function() {
+    $(".search").on("click", function () {
         searchQuery = $("#search-input").val();
         console.log(searchQuery);
         recordNumbers = $("#num-to-get").val();
         console.log(recordNumbers);
         yearStart = $("#start-year").val();
         console.log(yearStart);
+        yearEnd = $("#end-year").val();
+        console.log(yearEnd);
+
+        var queryURL = " https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + yearStart + "0101&end_date=" + yearEnd + "0101&q=" + searchQuery + "&api-key=pNI9zJwwGgvD913UF1vxDKz8J3FC6yde"
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+        })
+
     });
 
-    
 
 
-    $(".clear btn").on(function() {
+
+    $(".clear btn").on(function () {
         localStorage.clear();
         initPage()
     })
