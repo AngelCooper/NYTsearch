8255c73f22262bdea064aa1efbeb0ed8abf6ab36
AngelCooper <74269067+AngelCooper@users.noreply.github.com>
2021-01-12 13:17:03 -0600

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
 
+    //empty variable to hold search term
+    var searchQuery = ""
+    //empty variable to hold the number of records to show
+    var recordNumbers = ""
+    //empty variable to hold the number of records to the Start Year
+    var yearStart = ""
+    //empty variable to hold the number of records to the End Year
+    var yearEnd = ""
+    // We might need to append 0101 to the year
 
+
+    var queryURL = " https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + yearStart + "0101&end_date=" + yearEnd + "0101&q=" + searchQuery + "&api-key=pNI9zJwwGgvD913UF1vxDKz8J3FC6yde"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        // console.log(response)
+    })
     
     $(".clear btn").on(function() {
         localStorage.clear();
