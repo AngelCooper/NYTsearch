6552da3a12c016d99a1c1c0de7aa1708d6c50dbf
AngelCooper <74269067+AngelCooper@users.noreply.github.com>
2021-01-12 12:41:50 -0600

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
 
 
-    var generalSearch;      
+    var generalSearch;  
+    
+    $("").on("click", function(event) {
+       //add element in the quotes above and below
+        var search = $("").val();
+        generalSearch = //;
+
+        var queryUrl = //api url
+
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response)
+        });
+
+    });  
 
     
 })
